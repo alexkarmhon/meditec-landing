9894faab029b8b8b84f566d215f5914e4769e358
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2024-11-20 08:42:09 +0000

    Deploying to gh-pages from @ alexkarmhon/meditec-landing@3e2c70231e7692cf03386b30fa57069ca1fb4ad3 🚀

--- assets/index-7cc10175.js
+++ assets/index-7cc10175.js
@@ -40,3 +40,11 @@
   }
 })();
 const style = "";
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector("[data-menu-button]");
+  const mobileMenu = document.getElementById("mobile-menu");
+  menuButton.addEventListener("click", () => {
+    const isOpen = menuButton.classList.toggle("is-open");
+    mobileMenu.classList.toggle("is-open", isOpen);
+  });
+});
