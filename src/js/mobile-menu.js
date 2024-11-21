62f0c39834b9105332bc726c57dd0e2ec5f73745
alexkarmhon <89stigmat89@gmail.com>
2024-11-21 12:52:25 +0200

    Button icon(final)

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.querySelector("[data-menu-button]");
   const mobileMenu = document.getElementById("mobile-menu");
+  const buttonIcon = document.getElementsByClassName("mobile-menu-btn__icon");
 
   menuButton.addEventListener("click", () => {
     const isOpen = menuButton.classList.toggle("is-open"); // Додаємо/знімаємо клас `is-open`
     mobileMenu.classList.toggle("is-open", isOpen); // Синхронізуємо стан меню
+    buttonIcon.classList.toggle("is-open", isOpen);
   });
 });
