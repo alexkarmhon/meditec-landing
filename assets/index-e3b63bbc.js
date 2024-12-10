489c9ba3eab7e91300eca1110a8986f602fd2355
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2024-12-10 09:35:46 +0000

    Deploying to gh-pages from @ alexkarmhon/meditec-landing@e01f8bac9ebcbb4908c71643815db41bdedfd629 🚀

--- assets/index-e3b63bbc.js
+++ assets/index-e3b63bbc.js
@@ -51,3 +51,46 @@ document.addEventListener("DOMContentLoaded", () => {
     buttonIcon.classList.toggle("active", isOpen);
   });
 });
+(function() {
+  const fonts = ["cursive"];
+  let captchaValue = "";
+  function genCaptcha() {
+    let value = btoa(Math.random() * 1e9);
+    value = value.substr(0, 5 + Math.random() * 5);
+    captchaValue = value;
+  }
+  function setCaptcha() {
+    let html = captchaValue.split("").map((char) => {
+      const rotate = -20 + Math.random() * 30;
+      const font = Math.trunc(Math.random() * fonts.length);
+      return `<span
+            style="
+            transform:rotate(${rotate}deg);
+            font-family:${font[font]};
+            "
+           >${char} </span>`;
+    }).join("");
+    document.querySelector(".form__preview").innerHTML = html;
+  }
+  function initCaptcha() {
+    document.querySelector(".form__captcha--refresh").addEventListener("click", function(e) {
+      e.preventDefault();
+      genCaptcha();
+      setCaptcha();
+    });
+    genCaptcha();
+    setCaptcha();
+  }
+  initCaptcha();
+  document.querySelector(".form__button").addEventListener("click", function(e) {
+    e.preventDefault();
+    let inputcaptchavalue = document.querySelector(
+      ".form__input--captcha"
+    ).value;
+    if (inputcaptchavalue === captchaValue) {
+      alert("Ok");
+    } else {
+      alert("Invalid Captcha");
+    }
+  });
+})();
