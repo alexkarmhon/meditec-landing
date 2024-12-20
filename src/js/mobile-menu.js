13969dfc2184dd829a070f7da5dcbcbd571123ec
alexkarmhon <89stigmat89@gmail.com>
2024-12-20 21:59:26 +0200

    Mobile menu completed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuButton = document.querySelector("[data-menu-button]");
   const mobileMenu = document.getElementById("mobile-menu");
+  const mobileMenuNav = document.querySelector(".mobile-menu__navigation");
   const buttonIcon = document.querySelector(".mobile-menu-btn__icon");
   const header = document.querySelector(".header");
 
@@ -12,12 +13,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // });
 
   mobileMenu.style.transform = `translateY(${header.offsetHeight}px)`;
-  mobileMenu.style.paddingBottom = `${header.offsetHeight + 20}px`;
+  mobileMenuNav.style.height = `calc(100vh - ${header.offsetHeight}px)`;
+  mobileMenu.style.paddingBottom = `${header.offsetHeight}px`;
 
   menuButton.addEventListener("click", () => {
     const isOpen = menuButton.classList.toggle("is-open");
     mobileMenu.classList.toggle("is-open", isOpen);
     buttonIcon.classList.toggle("not-active", !isOpen);
     buttonIcon.classList.toggle("active", isOpen);
+  });
+
+  mobileMenu.addEventListener("click", (e) => {
+    if (e.target === e.currentTarget) {
+      const isOpen = menuButton.classList.toggle("is-open");
+      mobileMenu.classList.toggle("is-open", isOpen);
+      buttonIcon.classList.toggle("not-active", !isOpen);
+      buttonIcon.classList.toggle("active", isOpen);
+    }
   });
 });
