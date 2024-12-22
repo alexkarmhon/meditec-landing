210b3ebf65bbbc9532287f95a6a30c6741fad9ff
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2024-12-22 19:12:26 +0000

    Deploying to gh-pages from @ alexkarmhon/meditec-landing@fe50cea120867de9996d60d4a1fcd3d425ed728e 🚀

--- assets/index-161f8d81.js
+++ assets/index-161f8d81.js
@@ -159,4 +159,15 @@ images.forEach((img, index) => {
     currentSlide(index + 1);
   });
 });
+let lastScrollY = window.scrollY;
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  const currentScrollY = window.scrollY;
+  if (currentScrollY > lastScrollY) {
+    header.classList.add("hidden");
+  } else {
+    header.classList.remove("hidden");
+  }
+  lastScrollY = currentScrollY;
+});
 console.log("Hello. If you see this string - code is normal");
