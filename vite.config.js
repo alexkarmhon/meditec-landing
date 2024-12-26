b735916eed2f332cb6044f92e5cff5812fb11d61
alexkarmhon <89stigmat89@gmail.com>
2024-12-26 09:19:02 +0200

    changed base path1

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: "/meditec-landing",
+  base: "/alexkarmhon.github.io/meditec-landing",
   plugins: [
     ViteImageOptimizer({
       png: {
