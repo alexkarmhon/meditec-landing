d1307b4fbe0c4bcdb48864b77ddd2b89653de188
alexkarmhon <89stigmat89@gmail.com>
2024-12-26 11:04:34 +0200

    ???

--- src/js/productsListBuilder.js
+++ src/js/productsListBuilder.js
@@ -9,7 +9,7 @@ const cards = products
                   <div id="${id}" class="product-card">
                     <div class="product-card__image">
                         <img
-                          src=${import.meta.env.VITE_BASE_PATH}${image}
+                          src=${image}
                           alt=${title}                        
                           loading="lazy"
                           data-lazy="true"
