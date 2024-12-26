ea61fd41d7ea7e10e4ef1bf2c4ac19bf9ea61fd1
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2024-12-26 07:12:05 +0000

    Deploying to gh-pages from @ alexkarmhon/meditec-landing@59142e17984c79b7711fa0e4911b671abba86268 🚀

--- assets/index-8c9d3424.js
+++ assets/index-8c9d3424.js
@@ -155,7 +155,7 @@ const cards = products.map(
                   <div id="${id}" class="product-card">
                     <div class="product-card__image">
                         <img
-                          src=/meditec-landing${image}
+                          src=${image}
                           alt=${title}                        
                           loading="lazy"
                           data-lazy="true"
@@ -193,7 +193,7 @@ const renderCardById = (cardId) => {
   productCardModalContent.innerHTML = `
   <div class="production__modal-image-box">
     <img
-      src=/meditec-landing${image}
+      src=${image}
       alt=${title}
       loading="lazy"
       data-lazy="true"
