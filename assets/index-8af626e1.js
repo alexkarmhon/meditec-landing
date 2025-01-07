1f7e63ca9a01f67dcde4609ff837819687d53961
alexkarmhon <132042184+alexkarmhon@users.noreply.github.com>
2025-01-07 19:23:26 +0000

    Deploying to gh-pages from @ alexkarmhon/meditec-landing@2fbc55c12754f540799390a28fb0da6627dfa43a 🚀

--- assets/index-8af626e1.js
+++ assets/index-8af626e1.js
@@ -466,4 +466,22 @@ form.addEventListener("submit", (e) => {
   sendContact(contactUserData);
   contactForm.reset();
 });
+const upBtn = document.getElementById("up-btn");
+const scrollFunction = () => {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    upBtn.style.display = "flex";
+  } else {
+    upBtn.style.display = "none";
+  }
+};
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
+window.onscroll = function() {
+  scrollFunction();
+};
+upBtn.addEventListener("click", scrollToTop);
 console.log("Hello. If you see this string - code is normal");
